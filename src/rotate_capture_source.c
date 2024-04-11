ff81008b81c3bf46df6e08e0120486ec0096b7fa
Austin Songer <a.songer@protonmail.com>
2024-04-10 19:20:48 -0500

    Create rotate_capture_source.c

---
+++ src/rotate_capture_source.c
@@ -0,0 +1,5 @@
+// rotate_capture_source.c
+#include "rotate_capture_source.h"
+
+// Implement the functions declared in rotate_capture_source.h
+// Include the logic for handling source rotation and timers here
